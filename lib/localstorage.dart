268f542783d5486ec723076ac249726343a34d18
Kittitschai Yimvuthikul <kittitschai.yimvuthikul@fhnw.ch>
2021-09-24 15:49:27 +0200

    feat: export api

--- lib/localstorage.dart
+++ lib/localstorage.dart
@@ -2,3 +2,26 @@
 ///
 /// The database provided in this class is suitable for partial replication.
 library localstorage;
+
+export 'src/db/data/Field.dart';
+export 'src/db/data/Node.dart';
+export 'src/db/data/NodeImpl.dart';
+export 'src/db/data/NodeValue.dart';
+export 'src/db/data/NodeValueImpl.dart';
+export 'src/db/data/SwitchableBoolean.dart';
+
+export 'src/db/mapper/AbstractMapper.dart';
+export 'src/db/mapper/AbstractSqlMapper.dart';
+export 'src/db/mapper/DummyMapper.dart';
+export 'src/db/mapper/SqliteMapper.dart';
+
+export 'src/db/GenericController.dart';
+export 'src/db/StorageMapper.dart';
+
+export 'src/ChangeRegistrar.dart';
+export 'src/EventType.dart';
+export 'src/SearchCriteria.dart';
+export 'src/StorageController.dart';
+export 'src/StorageException.dart';
+export 'src/StorageListener.dart';
+export 'src/Visibility.dart';
